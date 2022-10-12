776ed319038ab4cc0783c959765689712eb3238c
kethnal <kaseyvaldez@hotmail.com>
2022-10-12 15:36:15 -0700

    handle character countdown, turn red when number is negative

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+  $('#tweet-text').on('input', function() {
+
+    let totalChar = 140 - $(this).val().length;
+    const $target = $(this).next().children().last();
+
+    if (totalChar < 0) {
+      $target.addClass('counter-over');
+    } else {
+      $target.removeClass('counter-over');
+    }
+
+    $target.text(totalChar);
+  });
+
+  
+});
