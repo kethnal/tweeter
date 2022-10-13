796a6b98542df3c6da5fac944e4ab5b2bd22ba42
kethnal <kaseyvaldez@hotmail.com>
2022-10-12 17:03:07 -0700

    add details to tweet display- icons change colour when hovering, tweet box shows drop shadow when hovering

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,21 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+
+  $('article').on('mouseover', function() {
+    $(this).addClass('hover-art');
+  });
+
+  $('article').on('mouseleave', function() {
+    $(this).removeClass('hover-art');
+  });
+
+  $('.icon').on('mouseover', function() {
+    $(this).addClass('hover-icon');
+  });
+
+  $('.icon').on('mouseleave', function() {
+    $(this).removeClass('hover-icon');
+  });
+});
